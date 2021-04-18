7edb63da35e20e0437ebad59aa957b217706fb68
Ana-Maria Melinte <anma.melinte@gmail.com>
2021-04-18 23:33:59 +0300

    Depth / Magnitude / Longitude / Latitude - correlation plots

--- Romania Earthquakes - Exploratory data analysis.r
+++ Romania Earthquakes - Exploratory data analysis.r
@@ -1,4 +1,29 @@
+attach(mtcars)
+
 setwd("C:/Users/Ana-Maria's Laptop/Documents/GitHub/infoVisualization")
 csv_file = "eartquakes_Romania.csv"
-dataset <- read.csv(file = csv_file)
-write.csv(dataset)
+df <- read.csv(file = csv_file)
+write.csv(df)
+
+year <- df&time
+
+dataset_agg <- aggregate(df$depth,by=list(df$Date),sum,na.rm=TRUE)
+
+
+df <- cbind(df, year)
+
+
+plot(df$depth, df$mag, main="Scatterplot - Depth & magnitude correlation",
+     xlab="Earthquake depth", ylab="Earthquake maginitude", pch=19)
+
+plot(df$longitude, df$mag, main="Scatterplot - Longitude & magnitude correlation",
+     xlab="Earthquake longitude", ylab="Earthquake maginitude", pch=19)
+
+plot(df$latitude, df$mag, main="Scatterplot - Latitude & magnitude correlation",
+     xlab="Earthquake latitude", ylab="Earthquake maginitude", pch=19)
+
+plot(df$longitude, df$depth, main="Scatterplot - Longitude & magnitude correlation",
+     xlab="Earthquake longitude", ylab="Earthquake depth", pch=19)
+
+plot(df$latitude, df$depth, main="Scatterplot - Latitude & latitude correlation",
+     xlab="Earthquake latitude", ylab="Earthquake depth", pch=19)
